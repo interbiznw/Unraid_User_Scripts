fa5e55bdc038f1f577c0d9b091e317129ccadf8f
J62 <j.owens@interbiznw.com>
2021-10-05 19:49:36 -0700

    Update APC_UPS.php

--- APC_UPS.php
+++ APC_UPS.php
@@ -2,6 +2,14 @@
 # Unraid User Script for sending UPS monitoring data to InfluxDb>2.0+ for display in grafana
 # Created/Modified by Jamie Owens/Interbiznw.com
 # previous reference for Old influxdb https://technicalramblings.com/blog/setting-grafana-influxdb-telegraf-ups-monitoring-unraid
+#
+# Initial steps:
+# 1) create bucket in influxdb2.0+ and enter id below.
+# 2) take note of ORG id # and enter below.
+# 3) create token with read/write access to previously created bucket only, place that token in the place below marked 'YOUR_API_TOKEN'
+# 4) place in UNRAID User scripts, and set custom schedule of '* * * * *' to run every minute
+#
+#
 
 <?php
 
